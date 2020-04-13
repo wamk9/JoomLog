f6326b76d3b7e2fbad943789ab5be391de215842
Wesley Matos <wamk-9@hotmail.com>
2020-04-13 17:36:28 -0300

    [CHANGED] Front-end view updated and more language strings (en-GB)

--- site/views/joomlog/tmpl/default.php
+++ site/views/joomlog/tmpl/default.php
@@ -11,12 +11,57 @@
 defined('_JEXEC') or die('Restricted access');
 ?>
 
-<h1>Changelog</h1>
+<style>
+.my-joomlog-0
+{
+	margin-top: 0 !important;
+	margin-bottom: 0 !important;
+}
+
+.ml-joomlog-2 { margin-left: .5rem !important; }
+.ml-joomlog-4 { margin-left: 1.5rem !important; }
+.ml-joomlog-5 { margin-left: 3rem !important; }
+
+.joomlog-version { font-size: 1.75rem; }
+.joomlog-logtype { font-size: 1.25rem; }
+.joomlog-log { font-size: 1rem; }
+
+hr.separator-joomlog 
+{
+	margin-top: 1rem;
+	margin-bottom: 1rem;
+}
+
+#joomlog
+{
+	margin-top: 15px;
+	margin-left: auto;
+	margin-right: auto;
+	max-width: 1000px;
+	width: 100%;
+}
+</style>
+
+<div id="joomlog">
+<h1><?php echo JText::_('COM_JOOMLOG_FRONTEND_CHANGELOG_TITLE'); ?></h1>
+<?php echo JText::_('COM_JOOMLOG_FRONTEND_CHANGELOG_DESC');; ?>
+
+<?php
+foreach($this->logType as $logType)
+{
+	$logType = get_object_vars($logType);
+	
+	echo '<p class="my-joomlog-0">'.$logType['title'].' - '.$logType['description'].'</p>';
+}
+?>
+
+<hr>
+
 <?php 
 foreach($this->versions as $version)
 {
 	$version = get_object_vars($version);
-	echo '<h2 class="ml-2 h3">'.$version['version'].' ('.$version['createdDate'].')</h2>';
+	echo '<h2 class="ml-joomlog-2 joomlog-version">'.$version['version'].' ('.$version['createdDate'].')</h2>';
 	
 	foreach($this->logType as $logType)
 	{
@@ -31,12 +76,13 @@ foreach($this->versions as $version)
 			{
 				if($logType['DisplayTitle'])
 				{ 
-					echo '<h2 class="ml-4 h5">'.$logType["title"].'</h2>'; 
+					echo '<h3 class="ml-joomlog-4 joomlog-logtype">'.$logType["title"].'</h3>'; 
 					$logType['DisplayTitle'] = false;
 				}
-				echo '<p class="ml-5 h6">'.$logs['title'].'</p>';
+				echo '<p class="ml-joomlog-5 joomlog-log">'.$logs['title'].'</p>';
 			}
 		}
 	}	
 }
 ?>
+</div>
